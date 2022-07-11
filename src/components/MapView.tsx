4d309d5ef052a17fa0909746a7d8f5a6ab25ac24
OzkrMonroy <oscarmonroy657@gmail.com>
2022-07-11 16:13:09 -0600

    Display the map is ready.

--- src/components/MapView.tsx
+++ src/components/MapView.tsx
@@ -1,16 +1,36 @@
-import { useContext } from "react"
+import { Map } from "mapbox-gl";
+import { useContext, useEffect, useRef } from "react"
 import { PlacesContext } from "../context/places"
 import { Loading } from "./Loading"
 
 export const MapView = () => {
-    const { isLoading, userLocation } = useContext(PlacesContext)
+    const { isLoading, userLocation } = useContext(PlacesContext);
+    const mapDiv = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        if(!isLoading){
+            const map = new Map({
+                container: mapDiv.current!, // container ID
+                style: 'mapbox://styles/mapbox/streets-v11', // style URL
+                center: userLocation, // starting position [lng, lat]
+                zoom: 9, // starting zoom
+                projection: { name: 'globe' } // display the map as a 3D globe
+                });
+        }
+    }, [isLoading])
 
     if(isLoading){
         return <Loading/>
     }
 
     return(
-        <div>
+        <div ref={mapDiv} style={{
+            height: '100vh',
+            left: '0',
+            position: 'fixed',
+            top: '0',
+            width: '100vw'
+        }}>
             {userLocation?.join(',')}
         </div>
     )
--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client';
 import { MapsApp } from './MapsApp';
 import './index.css'
 
+import mapboxgl from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
+ 
+mapboxgl.accessToken = 'pk.eyJ1Ijoib3prcm1vbnJveSIsImEiOiJjaXlmNWJvNDkwMGFkMzJvZWt1NDJnNzlvIn0.V_YsQfwis9wCTn2-vh4zmw';
+
 if(!navigator.geolocation){
   alert('Geolocation is not supported.');
   throw new Error('Geolocation is not supported for your shit!!')
