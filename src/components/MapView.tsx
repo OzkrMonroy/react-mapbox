5034fbec60788a093fd2eb29dc4cadbe653b3dd4
Oscar Monroy <oscarmonroy657@gmail.com>
2022-07-17 16:30:19 -0600

    Fixed the author name.

--- src/components/MapView.tsx
+++ src/components/MapView.tsx
@@ -1,4 +1,6 @@
-import { Map } from "mapbox-gl";
+/*eslint import/no-webpack-loader-syntax: off */
+//@ts-ignore
+import { Map } from "!mapbox-gl";
 import { useContext, useEffect, useRef } from "react"
 import { MapContext } from "../context/map";
 import { PlacesContext } from "../context/places"
